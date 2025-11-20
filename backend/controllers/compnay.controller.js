2fe284ca502d39d19fee8f1335358f0e7ed7d90a
prerna rajput <rajputprerna1503@gmail.com>
2025-11-21 01:12:55 +0530

    clundinary

--- backend/controllers/compnay.controller.js
+++ backend/controllers/compnay.controller.js
@@ -1,4 +1,6 @@
 import { Company } from "../models/company.model.js";
+import cloudinary from "../utils/cloudinary.js";
+import getDataUri from "../utils/getDataUri.js";
 
 // registering company
 export const registerCompany = async (req, res) => {
@@ -90,8 +92,11 @@ export const updateCompany = async (req, res) => {
     const file = req.file;
 
     // cloudinary
+    const fileUri = getDataUri(file);
+    const cloudResponse = await cloudinary.uploader.upload(fileUri.content);
+    const logo = cloudResponse.secure_url;
 
-    const updateData = { name, description, website, location };
+    const updateData = { name, description, website, location, logo };
     const company = await Company.findByIdAndUpdate(req.params.id, updateData, {
       new: true,
     });
--- backend/utils/getDataUri.js
+++ backend/utils/getDataUri.js
@@ -3,6 +3,13 @@ import DataUriParser from "datauri/parser.js";
 import path from "path";
 
 const getDataUri = (file) => {
+  if (!file) {
+    throw new Error("No file provided to getDataUri");
+  }
+  if (!file.originalname || !file.buffer) {
+    throw new Error("Invalid file object passed to getDataUri");
+  }
+
   const parser = new DataUriParser();
   const extName = path.extname(file.originalname).toString();
   return parser.format(extName, file.buffer);
@@ -10,10 +17,9 @@ const getDataUri = (file) => {
 
 export default getDataUri;
 
-
-// 
+//
 // Multer ke memory storage me:
-// 
+//
 // file = {
 //   originalname: "resume.pdf",
 //   mimetype: "application/pdf",
