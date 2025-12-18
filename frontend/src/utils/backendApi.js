859614b08cedde6e32f05c357cf792ae42604e02
prerna rajput <rajputprerna1503@gmail.com>
2025-12-18 18:28:25 +0530

    api

--- frontend/src/utils/backendApi.js
+++ frontend/src/utils/backendApi.js
@@ -13,7 +13,8 @@
 
 const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
-export const USER_API_END_POINT = `${BASE_URL}/v1/user`;
-export const JOB_API_END_POINT = `${BASE_URL}/v1/job`;
-export const APPLICATION_API_END_POINT = `${BASE_URL}/v1/application`;
-export const COMPANY_API_END_POINT = `${BASE_URL}/v1/company`;
+export const USER_API_END_POINT = `${BASE_URL}/api/v1/user`;
+export const JOB_API_END_POINT = `${BASE_URL}/api/v1/job`;
+export const APPLICATION_API_END_POINT = `${BASE_URL}/api/v1/application`;
+export const COMPANY_API_END_POINT = `${BASE_URL}/api/v1/company`;
+
